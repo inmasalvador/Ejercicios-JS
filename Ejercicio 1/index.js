8622414d83c1df0ca3e2426f9db03ee54b5098f7
inmasalvador <macuforest@gmail.com>
2022-01-30 21:52:51 +0100

    Ejercicio1

--- Ejercicio 1/index.js
+++ Ejercicio 1/index.js
@@ -5,9 +5,19 @@ const volumen = 16*23*13;
 console.log('volumen', volumen + 'cm3');
 
 // A = al + 2 ab
-const areaLateral = 23*13
-const areaBase = 16*23
-const areaTotal = areaLateral + 2* areaBase
-console.log('areaTotal', areaTotal + 'cm2')
+const areaLateral = 23*13;
+const areaBase = 16*23;
+const areaTotal = areaLateral + 2* areaBase;
+console.log('areaTotal', areaTotal + 'cm2');
 
 
+// Generalización
+const base = 23;
+const altura = 13;
+const ancho = 16;
+
+const volumen2 = base * altura * ancho;
+console.log ('volumen2', volumen2);
+
+const area2 = base * altura + 2*base*ancho;
+console.log ('area2', area2)
