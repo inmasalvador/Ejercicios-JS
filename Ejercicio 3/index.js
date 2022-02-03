99518f8bb3ddff4bba6d37696ee8d60ba6bbb9c7
inmasalvador <macuforest@gmail.com>
2022-02-03 11:27:20 +0100

    Ejercicio3

---
+++ Ejercicio 3/index.js
@@ -0,0 +1,15 @@
+const data = ["hola", 2, 5, "adios"];
+
+//Determina cuál de los dos números que hay en el array es mayor.
+
+console.log (data[1] > data[2])
+
+
+
+//Determina el resultado de las cinco operaciones numéricas realizadas con los dos elementos numéricos.
+
+console.log (data[1] + data[2]);
+console.log (data[1] - data[2]);
+console.log (data[1] * data[2]);
+console.log (data[1] / data[2]);
+console.log (data[1] % data[2]);
