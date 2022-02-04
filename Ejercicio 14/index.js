37c4094f22f36367f0f88fb762ab4bae9d635e3e
inmasalvador <macuforest@gmail.com>
2022-02-04 14:51:22 +0100

    Ejercicio14

--- Ejercicio 14/index.js
+++ Ejercicio 14/index.js
@@ -1,19 +1,22 @@
- // Número de enlaces de la pagina
+window.onload = function() {
+     
+    // Número de enlaces de la pagina
 
- const enlaces = document.getElementsByTagName('a');
- enlaces.innerHTML = "Numero de enlaces = "+enlaces.length;
- 
- // Dirección a la que enlaza el penúltimo enlace
-
- 
- var mensaje = "El penultimo enlace apunta a: "+enlaces[enlaces.length-2];
- mensaje.innerHTML = mensaje.innerHTML + "<br/>" + mensaje;
-
-
-
-
- //Número de enlaces del tercer párrafo
+    var enlaces = document.getElementsByTagName('a');
+    enlaces.innerHTML = "Numero de enlaces = "+enlaces.length;
     
- const parrafos = document.getElementsByTagName('p');
- enlaces = parrafos[2].getElementsByTagName('a');
- parrafos.innerHTML = info.innerHTML + "<br/>" + "Numero de enlaces del tercer párrafo = "+enlaces.length;
+    // Dirección a la que enlaza el penúltimo enlace
+
+    
+    var mensaje = "El penultimo enlace apunta a: "+enlaces[enlaces.length-2].href;
+    mensaje.innerHTML = mensaje.innerHTML + "<br/>" + mensaje;
+
+
+
+
+    //Número de enlaces del tercer párrafo
+        
+    var parrafos = document.getElementsByTagName('p');
+    enlaces = parrafos[2].getElementsByTagName('a');
+    parrafos.innerHTML = parrafos.innerHTML + "<br/>" + "Numero de enlaces del tercer párrafo = "+enlaces.length;
+}
