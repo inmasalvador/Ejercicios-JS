91b20209a0decd3ef6d7fc8a63806eb3b289bb03
inmasalvador <macuforest@gmail.com>
2022-02-04 14:31:00 +0100

    Ejercicio14

--- Ejercicio 14/index.js
+++ Ejercicio 14/index.js
@@ -1,13 +1,19 @@
  // Número de enlaces de la pagina
 
- const element = document.getElementsByTagName('a');
+ const enlaces = document.getElementsByTagName('a');
+ enlaces.innerHTML = "Numero de enlaces = "+enlaces.length;
  
  // Dirección a la que enlaza el penúltimo enlace
 
- const mensaje = "El penultimo enlace apunta a:" +enlaces[enlaces.length-2].href;
  
- 
+ var mensaje = "El penultimo enlace apunta a: "+enlaces[enlaces.length-2];
+ mensaje.innerHTML = mensaje.innerHTML + "<br/>" + mensaje;
+
+
+
+
  //Número de enlaces del tercer párrafo
     
  const parrafos = document.getElementsByTagName('p');
  enlaces = parrafos[2].getElementsByTagName('a');
+ parrafos.innerHTML = info.innerHTML + "<br/>" + "Numero de enlaces del tercer párrafo = "+enlaces.length;
